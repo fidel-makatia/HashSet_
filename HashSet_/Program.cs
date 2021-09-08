7c068369211f99079757638ef2acf65836b5a076
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-08 22:51:02 +0300

    Add project files.

---
+++ HashSet_/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashSet_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
